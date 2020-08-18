9b22e88796c5cfc2f71443f48365028f82c22d37
Tran Le Hoang <lehoangtran289@gmail.com>
2020-08-18 21:48:02 +0700

    add models

---
+++ src/main/java/com/hust/baseweb/model/DPerson.java
@@ -0,0 +1,4 @@
+package com.hust.baseweb.model;
+
+public class DPerson {
+}
--- src/main/java/com/hust/baseweb/service/UserService.java
+++ src/main/java/com/hust/baseweb/service/UserService.java
@@ -1,8 +1,14 @@
 package com.hust.baseweb.service;
 
 import com.hust.baseweb.entity.UserLogin;
+import com.hust.baseweb.model.DPerson;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService {
     UserLogin findById(String userLoginId);
-    
+
+    DPerson findByPartyId(String parytyId);
+
+
 }
