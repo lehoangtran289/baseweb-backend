5eaf82a95992b6410105b0f4d7c48c8e1faeb878
Tran Le Hoang <lehoangtran289@gmail.com>
2020-08-31 14:10:01 +0700

    test @Query of ManyToMany relationship

--- src/main/java/com/hust/baseweb/test/AuthorRepo.java
+++ src/main/java/com/hust/baseweb/test/AuthorRepo.java
@@ -2,11 +2,15 @@ package com.hust.baseweb.test;
 
 import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.core.types.dsl.StringPath;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.querydsl.binding.SingleValueBinding;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -20,6 +24,10 @@ public interface AuthorRepo extends JpaRepository<Author, Long>, QuerydslPredica
 
     @RestResource(path = "nameStartsWith")
     Optional<Author> findByAuthorNameStartsWith(String authorName);
+
+    @RestResource(path = "findByBookId")
+    @Query("select a from Author a join a.books b where b.bookId = ?1")
+    Page<Author> findByBookId(Long bookId, Pageable pageable);
 
     @Override
     default void customize(final QuerydslBindings bindings, final QAuthor root) {
