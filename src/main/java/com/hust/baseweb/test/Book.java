61f937a6602e4591bded1dcf87bc183099e236b4
Tran Le Hoang <lehoangtran289@gmail.com>
2020-08-31 11:34:35 +0700

    test RepoRestResource and QueryDsl

---
+++ src/main/java/com/hust/baseweb/test/Book.java
@@ -0,0 +1,34 @@
+package com.hust.baseweb.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bookId;
+
+    @NotNull
+    private String title;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "book_author", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private List<Author> authors;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+}
