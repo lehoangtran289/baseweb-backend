61d723386c2bf4c3008b55d26450c82c7956769d
Tran Le Hoang <lehoangtran289@gmail.com>
2020-10-13 16:30:37 +0700

    update rps

---
+++ src/main/java/com/hust/baseweb/application/covidtracker/models/DataDaily.java
@@ -0,0 +1,10 @@
+package com.hust.baseweb.application.covidtracker.models;
+
+import java.util.Date;
+
+public class DataDaily {
+    private Date date;
+    private int totalCases;
+    private int totalDeaths;
+    private int totalRecovers;
+}
